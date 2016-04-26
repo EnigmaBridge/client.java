304ccc3d8080ecbd624c10b69a093b6307c37528
Dusan Klinec <dusan.klinec@gmail.com>
2016-04-26 19:16:53 +0200

    missing process data util added

---
+++ src/main/java/com/enigmabridge/comm/EBProcessDataUtils.java
@@ -0,0 +1,8 @@
+package com.enigmabridge.comm;
+
+/**
+ * Created by dusanklinec on 26.04.16.
+ */
+public class EBProcessDataUtils {
+    public static final String PROCESS_DATA_CIPHER = "ProcessDataV1";
+}
