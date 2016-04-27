4e1f3773ce046af1042aa09864ce68ea2508aba2
Dusan Klinec <dusan.klinec@gmail.com>
2016-04-27 18:36:59 +0200

    UOInfo interface comments

--- src/main/java/com/enigmabridge/UserObjectInfo.java
+++ src/main/java/com/enigmabridge/UserObjectInfo.java
@@ -3,12 +3,44 @@ package com.enigmabridge;
 import java.io.Serializable;
 
 /**
+ * API for UserObject (UO) Info.
+ * Provides information to make use of EB user objects.
+ *
  * Created by dusanklinec on 26.04.16.
  */
 public interface UserObjectInfo extends Serializable {
+    /**
+     * Returns User object handle / ID.
+     * @return UO ID
+     */
      long getUoid();
-     String getApiKey();
+
+    /**
+     * Returns user object type.
+     * Required for API Token generation.
+     * @return UO type
+     */
+    long getUserObjectType();
+
+     /**
+      * Communication encryption&MAC keys.
+      * UO-specific. Strictly required for UO use.
+      * @return EBCommKeys
+      */
      EBCommKeys getCommKeys();
-     long getUserObjectType();
-     EBEndpointInfo getEndpointInfo();
+
+    /**
+     * Returns API key to EB API access.
+     * API key can be shared among several UOs.
+     * Not UO specific.
+     * @return API key
+     */
+     String getApiKey();
+
+    /**
+     * EB Endpoint identification.
+     * Not UO specific.
+     * @return endpoint info
+     */
+    EBEndpointInfo getEndpointInfo();
 }
