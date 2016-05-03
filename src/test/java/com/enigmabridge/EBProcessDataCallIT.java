8e5ea017402deeb444223d7099ac5c6ee3ab4eb5
Dusan Klinec <dusan.klinec@gmail.com>
2016-05-03 18:02:37 +0200

    ProcessData integration test extended

--- src/test/java/com/enigmabridge/EBProcessDataCallIT.java
+++ src/test/java/com/enigmabridge/EBProcessDataCallIT.java
@@ -83,11 +83,73 @@ public class EBProcessDataCallIT {
             assertTrue(response.getRawResponse().isSuccessful(), "HTTP request was not successful");
             assertEquals(response.getProtectedData(), EBUtils.hex2byte("95c6bb9b6a1c3835f98cc56087a03e82"));
 
+            // Try with POST method.
+            settings.setMethod(EBCommUtils.METHOD_POST);
+            final EBProcessDataCall call2 = new EBProcessDataCall.Builder()
+                    .setEngine(engine)
+                    .setSettings(settings)
+                    .setUo(uo)
+                    .setProcessFunction(EBRequestTypes.PLAINAES)
+                    .build();
+
+            final EBProcessDataResponse response2 = call2.doRequest(EBUtils.hex2byte("6bc1bee22e409f96e93d7e117393172a"));
+            assertNotNull(response2, "Response is null");
+            assertNotNull(response2.getRawResponse(), "Raw response is null");
+            assertTrue(response2.isCodeOk(), "Response code is not OK");
+            assertTrue(response2.getRawResponse().isSuccessful(), "HTTP request was not successful");
+            assertEquals(response2.getProtectedData(), EBUtils.hex2byte("95c6bb9b6a1c3835f98cc56087a03e82"));
+
+
         } catch (Exception ex){
-            LOG.error("Exception in get import keys call", ex);
+            LOG.error("Exception in ProcessData(PLAINAES, data)", ex);
             assertTrue(false);
         }
 
         LOG.trace("### UT ## EBProcessDataCallIT::testAES ## END ###");
+    }
+
+    @Test(groups = {"integration"}) //, timeOut = 100000
+    public void testRSA2k() throws Exception {
+        LOG.trace("### UT ## EBProcessDataCallIT::testRSA2k ## BEGIN ###");
+
+        try {
+            final EBConnectionSettings settings = new EBConnectionSettings()
+                    .setMethod(EBCommUtils.METHOD_POST)
+                    .setTrust(trust);
+
+            final EBCommKeys ck = new EBCommKeys()
+                    .setEncKey("1234567890123456789012345678901234567890123456789012345678901234")
+                    .setMacKey("2224262820223456789012345678901234567890123456789012345678901234");
+
+            final UserObjectInfoBase uo = new UserObjectInfoBase.Builder()
+                    .setUoid(0x7654L)
+                    .setUserObjectType(1)
+                    .setApiKey(apiKey)
+                    .setEndpointInfo(endpoint)
+                    .setCommKeys(ck)
+                    .build();
+
+            final EBProcessDataCall call = new EBProcessDataCall.Builder()
+                    .setEngine(engine)
+                    .setSettings(settings)
+                    .setUo(uo)
+                    .setProcessFunction(EBRequestTypes.PLAINAES)
+                    .build();
+
+            final String input  = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001";
+            final String output = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001";
+            final EBProcessDataResponse response = call.doRequest(EBUtils.hex2byte(input));
+            assertNotNull(response, "Response is null");
+            assertNotNull(response.getRawResponse(), "Raw response is null");
+            assertTrue(response.isCodeOk(), "Response code is not OK");
+            assertTrue(response.getRawResponse().isSuccessful(), "HTTP request was not successful");
+            assertEquals(response.getProtectedData(), EBUtils.hex2byte(output));
+
+        } catch (Exception ex){
+            LOG.error("Exception in ProcessData(RSA2048, data)", ex);
+            assertTrue(false);
+        }
+
+        LOG.trace("### UT ## EBProcessDataCallIT::testRSA2k ## END ###");
     }
 }
