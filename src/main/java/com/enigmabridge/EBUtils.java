d2c8729863b68c6d7a9d1e90adce074f8fb3119c
Dusan Klinec <dusan.klinec@gmail.com>
2016-05-04 17:33:02 +0200

    PKCS1.5 padding added + string.repeat util added

--- src/main/java/com/enigmabridge/EBUtils.java
+++ src/main/java/com/enigmabridge/EBUtils.java
@@ -34,6 +34,10 @@ public class EBUtils {
         return r.toString();
     }
 
+    public static String repeat(String s, int n) {
+        return new String(new char[n]).replace("\0", s);
+    }
+
     /**
      * Tries to extract json parameter as an integer.
      * @param json
