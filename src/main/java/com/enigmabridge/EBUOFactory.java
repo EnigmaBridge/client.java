8db878946c5604dbbde4e661526f29f817b42f7a
Dusan Klinec <dusan.klinec@gmail.com>
2016-05-04 20:12:30 +0200

    UO Key factory added - ToDo class
    - create instances of keys like PKCS11 SUN Provider does.
    - allow to build a key from KeySpec. KeySpec needs to be defined to specify our key accurately enough (UO ID, UO Type, comm keys, ...)

---
+++ src/main/java/com/enigmabridge/EBUOFactory.java
@@ -0,0 +1,7 @@
+package com.enigmabridge;
+
+/**
+ * Created by dusanklinec on 03.05.16.
+ */
+public class EBUOFactory {
+}
---
+++ src/main/java/com/enigmabridge/EBUOKeyFactory.java
@@ -0,0 +1,9 @@
+package com.enigmabridge;
+
+/**
+ * Created by dusanklinec on 03.05.16.
+ */
+public class EBUOKeyFactory {
+    // TODO: build keys from keySpecs.
+    // TODO: Key specs can be configurable from JSON, builder interface. Would contain UOid, UOtype...
+}
