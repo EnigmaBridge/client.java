86174dda37e826eadfa7d7a56459a2f584605a42
Dusan Klinec <dusan.klinec@gmail.com>
2016-07-06 09:09:34 +0200

    key pair generator - prototype for RSA gen

--- client/src/main/java/com/enigmabridge/provider/EBKeyPairGenerator.java
+++ client/src/main/java/com/enigmabridge/provider/EBKeyPairGenerator.java
@@ -1,9 +1,13 @@
 package com.enigmabridge.provider;
 
 
-import com.enigmabridge.EBEngine;
-import com.enigmabridge.EBEngineReference;
+import com.enigmabridge.*;
+import com.enigmabridge.comm.EBCorruptedException;
+import com.enigmabridge.create.*;
+import com.enigmabridge.provider.rsa.EBRSAPrivateKey;
 import sun.security.rsa.RSAKeyFactory;
+
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.*;
 import java.security.spec.*;
@@ -79,7 +83,11 @@ public class EBKeyPairGenerator extends KeyPairGeneratorSpi {
     }
 
     private void checkKeySize(int keySize, AlgorithmParameterSpec params) throws InvalidAlgorithmParameterException {
-        if(this.algorithm.equals("RSA")) {
+        if("RSA".equals(this.algorithm)) {
+            if (keySize != 1024 && keySize != 2048){
+                throw new InvalidAlgorithmParameterException("RSA1024 and RSA2048 are supported");
+            }
+
             BigInteger var3 = this.rsaPublicExponent;
             if(params != null) {
                 var3 = ((RSAKeyGenParameterSpec)params).getPublicExponent();
@@ -90,6 +98,7 @@ public class EBKeyPairGenerator extends KeyPairGeneratorSpi {
             } catch (InvalidKeyException var5) {
                 throw new InvalidAlgorithmParameterException(var5.getMessage());
             }
+
         } else if(keySize < 512) {
             throw new InvalidAlgorithmParameterException("Key size must be at least 512 bit");
         }
@@ -97,14 +106,65 @@ public class EBKeyPairGenerator extends KeyPairGeneratorSpi {
 
     public KeyPair generateKeyPair() {
         if(this.algorithm.equals("RSA")) {
-            // TODO: generate.
+            final SecureRandom rand = new SecureRandom();
+            final byte[] encKey = new byte[32];
+            final byte[] macKey = new byte[32];
+            rand.nextBytes(encKey);
+            rand.nextBytes(macKey);
+
+            final int uoTypeFunction = this.keySize == 1024 ? UserObjectType.TYPE_RSA1024DECRYPT_NOPAD : UserObjectType.TYPE_RSA2048DECRYPT_NOPAD;
+            final EBEndpointInfo endpoint = engine.getDefaultSettings().getEndpointInfo();
+            EBUOGetTemplateRequest req = new EBUOGetTemplateRequest();
+            req.setType(uoTypeFunction);
+            req.setGenerationCommKey(Constants.GENKEY_CLIENT);
+            req.setGenerationAppKey(Constants.GENKEY_ENROLL_RANDOM);
+
+            EBCreateUOSimpleCall callBld = new EBCreateUOSimpleCall.Builder()
+                    .setEngine(engine)
+                    .setEndpoint(new EBEndpointInfo(endpoint.getScheme(), endpoint.getHostname(), 11182))
+                    .setRequest(req)
+                    .addKey(new EBUOTemplateKey(Constants.KEY_COMM_ENC, encKey))
+                    .addKey(new EBUOTemplateKey(Constants.KEY_COMM_MAC, macKey))
+                    .build();
+
+            try {
+                final EBCreateUOResponse response = callBld.create();
+
+                // Load key public parts.
+                final BigInteger exp = null; // TODO
+                final BigInteger mod = null; // TODO
+                final PublicKey rsa2kPubkey = EBProviderUtils.createRSAPublicKey(mod, exp);
+
+                // Create UOKey
+                final UserObjectKeyBase key = new UserObjectKeyBase.Builder()
+                        .setUoid(response.getHandle().getUoId())
+                        .setUserObjectType(response.getHandle().getUoType().getValue())
+                        .setCommKeys(new EBCommKeys(encKey, macKey))
+                        .build();
+
+                // Create Java RSA key - will be done with key specs.
+                final EBRSAPrivateKey rsa2kPrivKey = new EBRSAPrivateKey.Builder()
+                        .setPublicExponent(exp)
+                        .setModulus(mod)
+                        .setUo(key)
+                        .setEngine(engine)
+                        .build();
+
+                return new KeyPair(rsa2kPubkey, rsa2kPrivKey);
+
+            } catch (IOException e) {
+                throw new ProviderException("Create RSA key failed", e);
+            } catch (EBCorruptedException e) {
+                throw new ProviderException("RSA keys are supported only", e);
+            } catch (InvalidKeySpecException e) {
+                throw new ProviderException("Cannot create RSA pub key", e);
+            } catch (NoSuchAlgorithmException e) {
+                throw new ProviderException("Cannot create RSA pub key", e);
+            }
 
         } else {
-
             throw new ProviderException("RSA keys are supported only");
         }
-
-        return null;
     }
 }
 
