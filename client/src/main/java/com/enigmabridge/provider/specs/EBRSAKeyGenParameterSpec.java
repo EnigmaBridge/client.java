8f1a48bc8fbdee9e3ca5722acae1153b99da71af
Dusan Klinec <dusan.klinec@gmail.com>
2016-07-11 14:32:06 +0200

    RSA keygen parameter spec implements get template spec

--- client/src/main/java/com/enigmabridge/provider/specs/EBRSAKeyGenParameterSpec.java
+++ client/src/main/java/com/enigmabridge/provider/specs/EBRSAKeyGenParameterSpec.java
@@ -2,6 +2,7 @@ package com.enigmabridge.provider.specs;
 
 import com.enigmabridge.EBEngine;
 import com.enigmabridge.EBEngineReference;
+import com.enigmabridge.create.EBUOGetTemplateRequest;
 
 import java.math.BigInteger;
 import java.security.spec.RSAKeyGenParameterSpec;
@@ -11,8 +12,20 @@ import java.security.spec.RSAKeyGenParameterSpec;
  *
  * Created by dusanklinec on 28.06.16.
  */
-public class EBRSAKeyGenParameterSpec extends RSAKeyGenParameterSpec implements EBEngineReference {
+public class EBRSAKeyGenParameterSpec extends RSAKeyGenParameterSpec implements EBEngineReference, EBCreateUOTemplateSpec {
     private final EBEngine engine;
+    private EBUOGetTemplateRequest tplReq;
+
+    /**
+     * Constructs a new <code>RSAParameterSpec</code> object from the
+     * given keysize. Public exponent is 65537.
+     *
+     * @param keysize        the modulus size (specified in number of bits)
+     */
+    public EBRSAKeyGenParameterSpec(int keysize) {
+        super(keysize, F4);
+        engine = null;
+    }
 
     /**
      * Constructs a new <code>RSAParameterSpec</code> object from the
@@ -31,8 +44,23 @@ public class EBRSAKeyGenParameterSpec extends RSAKeyGenParameterSpec implements 
         this.engine = engine;
     }
 
+    public EBRSAKeyGenParameterSpec(int keysize, EBEngine engine) {
+        super(keysize, F4);
+        this.engine = engine;
+    }
+
+    public EBRSAKeyGenParameterSpec setTplReq(EBUOGetTemplateRequest tplReq) {
+        this.tplReq = tplReq;
+        return this;
+    }
+
     @Override
     public EBEngine getEBEngine() {
         return engine;
+    }
+
+    @Override
+    public EBUOGetTemplateRequest getTemplateRequest() {
+        return tplReq;
     }
 }
