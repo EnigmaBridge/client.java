185ee20e2090c5dbbd0c1988ff270cad1402a058
Dusan Klinec <dusan.klinec@gmail.com>
2016-07-18 21:16:23 +0200

    new UOSpecs added
    - for key factory, creating keys from definition

---
+++ client/src/main/java/com/enigmabridge/provider/specs/EBJSONEncodedUOKeySpec.java
@@ -0,0 +1,10 @@
+package com.enigmabridge.provider.specs;
+
+import java.security.spec.KeySpec;
+
+/**
+ * TODO: support key factory from serialized objects.
+ * Created by dusanklinec on 18.07.16.
+ */
+public class EBJSONEncodedUOKeySpec implements KeySpec {
+}
