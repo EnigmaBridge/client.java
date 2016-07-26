79ba58578d9606e62846ab06eb29bdd19547981a
Dusan Klinec <dusan.klinec@gmail.com>
2016-07-27 01:27:03 +0200

    mass comm enc IT updated

--- client/src/test/java/com/enigmabridge/EBProcessDataMassIT.java
+++ client/src/test/java/com/enigmabridge/EBProcessDataMassIT.java
@@ -99,14 +99,14 @@ public class EBProcessDataMassIT {
                 .build();
 
         // Independent test
-        for(int testIdx = 0; testIdx < 100; testIdx++) {
+        for(int testIdx = 0; testIdx < 10; testIdx++) {
             // Generate random input.
             final int length = 16 * (1 + engine.getRnd().nextInt(16));
             byte[] plaintext = new byte[length];
             engine.getRnd().nextBytes(plaintext);
 
             // Repeat same plaintext several times to test hypothesis it is independent on input data.
-            for(int repIdx=0; repIdx < 10; repIdx++){
+            for(int repIdx=0; repIdx < 5; repIdx++){
                 final EBProcessDataCall call = new EBProcessDataCall.Builder()
                         .setUo(uo)
                         .setEngine(engine)
@@ -144,64 +144,5 @@ public class EBProcessDataMassIT {
         }
 
         LOG.trace("### UT ## EBProcessDataMassIT::testAESManyTimes ## END ###");
-    }
-
-    @Test(groups = {"integration"}) //, timeOut = 100000
-    public void testRSA2kBasic() throws Exception {
-        basicRSATest(EBTestingUtils.UOID_RSA2k, 2048);
-    }
-
-    /**
-     * Performs very simple RSA OU test.
-     * @param uoid
-     * @throws IOException
-     * @throws EBCorruptedException
-     */
-    private void basicRSATest(long uoid, int bitLength) throws IOException, EBCorruptedException {
-        final UserObjectInfoBase uo = new UserObjectInfoBase.Builder()
-                .setUoid(uoid)
-                .setUserObjectType(1)
-                .setApiKey(apiKey)
-                .setEndpointInfo(endpoint)
-                .setCommKeys(ckRSA)
-                .build();
-
-        final EBProcessDataCall call = new EBProcessDataCall.Builder()
-                .setEngine(engine)
-                .setSettings(settings)
-                .setUo(uo)
-                .build();
-
-        // Test RSA_DEC(1) == 1 as (1^d) mod N = 1
-        final String input  = EBUtils.repeat("00", bitLength/8-1) + "01";
-        final String output = EBUtils.repeat("00", bitLength/8-1) + "01";
-        final EBProcessDataResponse response = call.doRequest(EBUtils.hex2byte(input));
-        testResponse(response);
-        assertEquals(response.getProtectedData(), EBUtils.hex2byte(output));
-
-        // Test RSA_DEC(0) == 0
-        final String input2  = EBUtils.repeat("00", bitLength/8);
-        final String output2 = EBUtils.repeat("00", bitLength/8);
-        final EBProcessDataResponse response2 = call.doRequest(EBUtils.hex2byte(input2));
-        testResponse(response2);
-        assertEquals(response2.getProtectedData(), EBUtils.hex2byte(output2));
-
-        // Test RSA_DEC(2) != 2
-        final String input3  = EBUtils.repeat("00", bitLength/8-1) + "02";
-        final String output3 = EBUtils.repeat("00", bitLength/8-1) + "02";
-        final EBProcessDataResponse response3 = call.doRequest(EBUtils.hex2byte(input3));
-        testResponse(response3);
-        assertNotEquals(response3.getProtectedData(), EBUtils.hex2byte(output2));
-    }
-
-    /**
-     * Basic test on the EB response.
-     * @param response
-     */
-    private void testResponse(EBResponse response){
-        assertNotNull(response, "Response is null");
-        assertNotNull(response.getRawResponse(), "Raw response is null");
-        assertTrue(response.getRawResponse().isSuccessful(), "HTTP request was not successful");
-        assertTrue(response.isCodeOk(), "Response code is not OK");
     }
 }
